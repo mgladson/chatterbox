7c24a991e59df46003c459edbf8fda5e41c0a37d
Matthew Gladson <m.gladson@block-farms.io>
2026-03-07 01:19:35 +0700

    no watermarks

--- src/chatterbox/tts_turbo.py
+++ src/chatterbox/tts_turbo.py
@@ -292,5 +292,5 @@ class ChatterboxTurboTTS:
             n_cfm_timesteps=2,
         )
         wav = wav.squeeze(0).detach().cpu().numpy()
-        watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
-        return torch.from_numpy(watermarked_wav).unsqueeze(0)
+        # watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
+        return torch.from_numpy(wav).unsqueeze(0)
